472ddffd6adcae00faada1598981849a43e88f01
vsgirelli <vavi_sg@hotmail.com>
2018-09-12 23:55:40 -0300

    Adding notes on the functions and some piece of code

--- src/cthread.c
+++ src/cthread.c
@@ -2,23 +2,44 @@
 
 /******************************************************************************
 Parâmetros:
-	start:	ponteiro para a função que a thread executará.
-	arg:	um parâmetro que pode ser passado para a thread na sua criação.
-	prio:	NÃO utilizado neste semestre, deve ser sempre zero.
+  start: ponteiro para a função que a thread executará.
+  arg: um parâmetro que pode ser passado para a thread na sua criação.
+  prio: valores numéricos 0, 1 e 2 (alta, média e baixa prioridade).
+        (código de erro para demais valores)
 Retorno:
-	Se correto => Valor positivo, que representa o identificador da thread criada
-	Se erro	   => Valor negativo.
+  Se correto => Valor positivo, que representa o identificador da thread criada
+  Se erro	   => Valor negativo.
+
+Observações:
+  * A função deve alocar TCBs e estruturas necessárias para gerência da thread.
+  * Ao final da criação da thread, ela deve ser inserida ao fim da lista de aptos (FIFO).
+
+  * A função main possui um comportamento diferente (seção 4 do enunciado).  
+
+  * Quando um thread de maior prioridade for criada, a thread criadora deve ser preemptada.
+
 ******************************************************************************/
 int ccreate (void* (*start)(void*), void *arg, int prio) {
+  /*
+   *if (prio != PRIO_0 && prio != PRIO_1 && prio != PRIO_2) {
+   *  return PRIO_ERROR;
+   *}
+   */
   return FUNC_NOT_IMPLEMENTED;
 }
 
 /******************************************************************************
 Parâmetros:
-	Sem parâmetros
+Sem parâmetros
 Retorno:
-	Quando executada corretamente: retorna 0 (zero)
-	Caso contrário, retorna um valor negativo.
+  Quando executada corretamente: retorna 0 (zero)
+  Caso contrário, retorna um valor negativo.
+
+Observações:
+  * Thread que executa essa função retorna para a fila de aptos.
+  
+  * Escalonador deve ser chamado para selecionar a próxima thread.
+
 ******************************************************************************/
 int cyield(void) {
   return FUNC_NOT_IMPLEMENTED;
@@ -26,11 +47,17 @@ int cyield(void) {
 
 /******************************************************************************
 Parâmetros:
-	tid: identificador da thread cuja prioridade será alterada (deixar sempre esse campo como NULL em 2018/02)
-	prio: nova prioridade da thread.
+  tid: identificador da thread cuja prioridade será alterada (deixar sempre esse campo como NULL em 2018/02)
+  prio: nova prioridade da thread.
 Retorno:
-	Quando executada corretamente: retorna 0 (zero)
-	Caso contrário, retorna um valor negativo.
+  Quando executada corretamente: retorna 0 (zero)
+  Caso contrário, retorna um valor negativo.
+
+Observações:
+  * Se a thread setar sua prioridade a um valor menor do que a prioridade de um
+  thread no estado apto, o escalonador deve ser chamado para preemptar a
+  thread chamadora.
+
 ******************************************************************************/
 int csetprio(int tid, int prio) {
   return FUNC_NOT_IMPLEMENTED;
@@ -38,10 +65,10 @@ int csetprio(int tid, int prio) {
 
 /******************************************************************************
 Parâmetros:
-	tid:	identificador da thread cujo término está sendo aguardado.
+  tid:identificador da thread cujo término está sendo aguardado.
 Retorno:
-	Quando executada corretamente: retorna 0 (zero)
-	Caso contrário, retorna um valor negativo.
+  Quando executada corretamente: retorna 0 (zero)
+  Caso contrário, retorna um valor negativo.
 ******************************************************************************/
 int cjoin(int tid) {
   return FUNC_NOT_IMPLEMENTED;
@@ -49,11 +76,11 @@ int cjoin(int tid) {
 
 /******************************************************************************
 Parâmetros:
-	sem:	ponteiro para uma variável do tipo csem_t. Aponta para uma estrutura de dados que representa a variável semáforo.
-	count: valor a ser usado na inicialização do semáforo. Representa a quantidade de recursos controlados pelo semáforo.
+  sem:ponteiro para uma variável do tipo csem_t. Aponta para uma estrutura de dados que representa a variável semáforo.
+  count: valor a ser usado na inicialização do semáforo. Representa a quantidade de recursos controlados pelo semáforo.
 Retorno:
-	Quando executada corretamente: retorna 0 (zero)
-	Caso contrário, retorna um valor negativo.
+  Quando executada corretamente: retorna 0 (zero)
+  Caso contrário, retorna um valor negativo.
 ******************************************************************************/
 int csem_init(csem_t *sem, int count) {
   return FUNC_NOT_IMPLEMENTED;
@@ -61,10 +88,10 @@ int csem_init(csem_t *sem, int count) {
 
 /******************************************************************************
 Parâmetros:
-	sem:	ponteiro para uma variável do tipo semáforo.
+  sem:ponteiro para uma variável do tipo semáforo.
 Retorno:
-	Quando executada corretamente: retorna 0 (zero)
-	Caso contrário, retorna um valor negativo.
+  Quando executada corretamente: retorna 0 (zero)
+  Caso contrário, retorna um valor negativo.
 ******************************************************************************/
 int cwait(csem_t *sem) {
   return FUNC_NOT_IMPLEMENTED;
@@ -72,10 +99,10 @@ int cwait(csem_t *sem) {
 
 /******************************************************************************
 Parâmetros:
-	sem:	ponteiro para uma variável do tipo semáforo.
+  sem:ponteiro para uma variável do tipo semáforo.
 Retorno:
-	Quando executada corretamente: retorna 0 (zero)
-	Caso contrário, retorna um valor negativo.
+  Quando executada corretamente: retorna 0 (zero)
+  Caso contrário, retorna um valor negativo.
 ******************************************************************************/
 int csignal(csem_t *sem) {
   return FUNC_NOT_IMPLEMENTED;
@@ -83,12 +110,12 @@ int csignal(csem_t *sem) {
 
 /******************************************************************************
 Parâmetros:
-	name:	ponteiro para uma área de memória onde deve ser escrito um string que contém os nomes dos componentes do grupo e seus números de cartão.
-		Deve ser uma linha por componente.
-	size:	quantidade máxima de caracteres que podem ser copiados para o string de identificação dos componentes do grupo.
+  name:ponteiro para uma área de memória onde deve ser escrito um string que contém os nomes dos componentes do grupo e seus números de cartão.
+  Deve ser uma linha por componente.
+  size:quantidade máxima de caracteres que podem ser copiados para o string de identificação dos componentes do grupo.
 Retorno:
-	Quando executada corretamente: retorna 0 (zero)
-	Caso contrário, retorna um valor negativo.
+  Quando executada corretamente: retorna 0 (zero)
+  Caso contrário, retorna um valor negativo.
 ******************************************************************************/
 int cidentify (char *name, int size) {
   return FUNC_NOT_IMPLEMENTED;
